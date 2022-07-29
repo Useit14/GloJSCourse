b63f08ee2d7ff453497e8b517f33a117e9ff2a59
Useit14 <useit14@inbox.ru>
2022-07-29 11:29:36 +0300

    Set value to variables and add new methods

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "GloJSCourse_Lesson02";
+let screens = "Simple, Hard, Interactive";
+let screenPrice = 14;
+let rollback = 100;
+let fullPrice = 200000;
+let adaptive = true;
 
-alert("Hello GLO with help alert");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("Hello GLO with help console");
+console.log(screens.length);
+
+console.log(
+  `Стоимость верстки экранов ${screenPrice} рублей и Стоимость разработки сайта ${fullPrice} рублей`
+);
+
+let lowScreens = screens.toLowerCase();
+let substrScreens = lowScreens.split(", ");
+console.log(substrScreens);
+
+let percentRollback = fullPrice * (rollback / 100);
+console.log(`Процент отката посреднику за работу ${percentRollback}`);
