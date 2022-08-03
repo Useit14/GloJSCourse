c48bb8ee8f15992488fdc4ff06749f27149eb3cd
Useit14 <useit14@inbox.ru>
2022-08-03 09:54:24 +0300

    Complete

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ let title = prompt("Как называется ваш проект?", `Сайт
 let screens = prompt("Какие типы экранов нужно разработать?", "Простые");
 let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
 let rollback = 100;
-let fullPrice = 200000;
 let adaptive = confirm("Нужен ли адаптив на сайте?");
-let percentRollback;
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "Тип1");
 let servicePrice1 = +prompt("Сколько это будет стоить?", "12000");
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "Тип2");
 let servicePrice2 = +prompt("Сколько это будет стоить?", "12000");
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-percentRollback = fullPrice * (rollback / 100);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let percentRollback = fullPrice * (rollback / 100);
 let servicePercentPrice = fullPrice - Math.ceil(fullPrice * (rollback / 100));
 
 if (fullPrice > 30000) {
