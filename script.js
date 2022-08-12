3c77ce3be4d2109393772477425cf81542188e76
Useit14 <useit14@inbox.ru>
2022-08-12 10:58:19 +0300

    Fix problem

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let ul6 = uls[2];
 books[1].after(books[0]);
 books[5].after(books[2]);
 books[0].after(books[4]);
-
 ul2Li[3].after(ul2Li[6]);
 ul2Li[6].after(ul2Li[8]);
 ul2Li[9].after(ul2Li[2]);
@@ -16,8 +15,11 @@ ul5Li[9].after(ul5Li[3]);
 ul5Li[3].after(ul5Li[4]);
 ul5Li[7].after(ul5Li[5]);
 
-ul6.insertAdjacentHTML("afterend", "<li>Глава 8: За пределами ES6</li>");
-debugger;
+ul6.insertAdjacentHTML("beforeend", "<li>Глава 8: За пределами ES6</li>");
+
+let ul6Li = uls[2].querySelectorAll("li");
+ul6Li[8].after(ul6Li[10]);
+
 let titleBook = books[4].querySelector("h2");
 let aTitleBook = titleBook.querySelector("a");
 aTitleBook.textContent = "Книга 3. this и Прототипы Объектов";
